ba3d12222990812d92d71a98c36a36c6b70aba8c
Binyameen <bmghafoor@gmail.com>
2023-08-01 22:23:41 -0400

    BFS

--- binary-search-tree.js
+++ binary-search-tree.js
@@ -166,7 +166,24 @@ class BinarySearchTree {
   /** bfs(): Traverse the array using BFS.
    * Return an array of visited nodes. */
 
-  bfs() {}
+  bfs() {
+    let arr = [];
+    let queue = [this.root];
+
+    while (queue.length > 0) {
+      let node = queue.shift();
+      arr.push(node.val);
+
+      if (node.left) {
+        queue.push(node.left);
+      }
+      if (node.right) {
+        queue.push(node.right);
+      }
+    }
+
+    return arr;
+  }
 
   /** Further Study!
    * remove(val): Removes a node in the BST with the value val.
